8eed17d87365010f8d10aa3382cff04d7254cd22
Olatunji Olayinka <olatunji.weber@gmail.com>
2024-03-05 12:05:44 +0200

    Update handler.php
    Fixed the issue with the "handler.php"

--- version2/handler.php
+++ version2/handler.php
@@ -1,18 +1,21 @@
 <?php
-if (isset($_POST['submit'])) {  //Check if Post form has been submitted
+session_start();
 
-    // Retrieve the input data into a variable
-    if (!empty($_POST['name']) && !empty($_POST['password'])) {
-        echo "All Fields must be filled please...";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['name']) && isset($_POST['password'])) {
+        $name = htmlspecialchars($_POST['name']);
+        $password = htmlspecialchars($_POST['password']);
+
+        if (!empty($name) && !empty($password)) {
+            echo "<h1>Hello there, $name, here are your details:>> </h1>
+<h2>Name:  $name </h2>
+<h2>Password:  $password</h2>";
+        } else {
+            echo "<h2>Please fill in all the fields.</h2>";
+        }
     } else {
-        $name = $_POST['name'];
-        $password = $_POST['password'];
+        echo "<h3>Please fill in all the fields.</h3>";
     }
-
-    // We'll process the form data by just echoing the submitted data.
-    echo "Hello there, " . $name . " here are your details: ";
-    echo "Name: " . htmlspecialchars($name) . "<br>";
-    echo "Password: " . htmlspecialchars($password);
 } else {
-    echo "Error: Invalid request method.";
+    echo "<h3>Invalid request method.</h3>";
 }
